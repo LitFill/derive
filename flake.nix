190fbaf6c74554b2d67ee8df1d2740c019325005
LitFill <marrazzy54@gmail.com>
2026-09-12 21:13:36 +0700

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "CLI for deriving some useful functions for Koka types and structs";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "derive";
+        version = "v1.1.2";
+
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          koka
+        ];
+
+        buildPhase = ''
+          koka -o build/derive app/main.kk
+        '';
+
+        installPhase = ''
+          mkdir -p $out/bin
+          cp build/derive $out/bin/
+        '';
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          koka
+        ];
+      };
+    };
+}
